322d63a9a5fd4a1a0007866a61f2e3073f8c366b
kamisama-haha <iamoktootoo@gmail.com>
2024-07-28 07:27:32 +0800

    Update navigation.BDVBLgCJ.js

--- navigation.BDVBLgCJ.js
+++ navigation.BDVBLgCJ.js
@@ -1,30 +1,31 @@
 import { d as H, r as F, q as E, ap as q, a as G, a0 as R, a1 as O, i as _ } from "./entry.Dv1IsjD4.js";
+import { m as y, e as K, b as D, c as j, d as N } from "./effect-fade.aa3pNKSS.js";
 
-const U = Symbol.for("nuxt:client-only"),
-      J = H({
-        name: "ClientOnly",
-        inheritAttrs: !1,
-        props: ["fallback", "placeholder", "placeholderTag", "fallbackTag"],
-        setup(l, { slots: e, attrs: C }) {
-          const d = F(!1);
-          return (
-            E(() => {
-              d.value = !0;
-            }),
-            q(U, !0),
-            (u) => {
-              var L;
-              if (d.value)
-                return (L = e.default) == null ? void 0 : L.call(e);
-              const o = e.fallback || e.placeholder;
-              if (o) return o();
-              const b = u.fallback || u.placeholder || "",
-                    h = u.fallbackTag || u.placeholderTag || "span";
-              return G(h, C, b);
-            }
-          );
-        },
-      });
+const U = Symbol.for("nuxt:client-only");
+
+const J = H({
+  name: "ClientOnly",
+  inheritAttrs: !1,
+  props: ["fallback", "placeholder", "placeholderTag", "fallbackTag"],
+  setup(l, { slots: e, attrs: C }) {
+    const d = F(!1);
+    return (
+      E(() => {
+        d.value = !0;
+      }),
+      q(U, !0),
+      (u) => {
+        var L;
+        if (d.value) return (L = e.default) == null ? void 0 : L.call(e);
+        const o = e.fallback || e.placeholder;
+        if (o) return o();
+        const b = u.fallback || u.placeholder || "",
+          h = u.fallbackTag || u.placeholderTag || "span";
+        return G(h, C, b);
+      }
+    );
+  },
+});
 
 const Q = R("promo", {
   state: () => ({
@@ -72,8 +73,11 @@ const Q = R("promo", {
       this.states = l;
     },
     setSingleState(l) {
-      this.states.length === 0 && (this.states = [l]);
-      this.states = this.states.map((e) => (e.promoId === l.promoId ? l : e));
+      if (this.states.length === 0) {
+        this.states = [l];
+      } else {
+        this.states = this.states.map((e) => (e.promoId === l.promoId ? l : e));
+      }
     },
     getReceiveKeysToday(l) {
       var C;
@@ -131,11 +135,7 @@ function V(l) {
   let b,
     h = 0;
   function L() {
-    return (
-      !e.params.pagination.el ||
-      !e.pagination.el ||
-      (Array.isArray(e.pagination.el) && e.pagination.el.length === 0)
-    );
+    return !e.params.pagination.el || !e.pagination.el || (Array.isArray(e.pagination.el) && e.pagination.el.length === 0);
   }
   function B(a, t) {
     const { bulletActiveClass: s } = e.params.pagination;
@@ -163,84 +163,30 @@ function V(l) {
     let s = e.pagination.el;
     s = y(s);
     let i, f;
-    const $ =
-      e.virtual && e.params.virtual.enabled
-        ? e.virtual.slides.length
-        : e.slides.length,
-      P = e.params.loop
-        ? Math.ceil($ / e.params.slidesPerGroup)
-        : e.snapGrid.length;
-    if (
-      (e.params.loop
-        ? ((f = e.previousRealIndex || 0),
-          (i =
-            e.params.slidesPerGroup > 1
-              ? Math.floor(e.realIndex / e.params.slidesPerGroup)
-              : e.realIndex))
-        : typeof e.snapIndex < "u"
-        ? ((i = e.snapIndex), (f = e.previousSnapIndex))
-        : ((f = e.previousIndex || 0), (i = e.activeIndex || 0)),
-      t.type === "bullets" && e.pagination.bullets && e.pagination.bullets.length > 0)
-    ) {
+    const $ = e.virtual && e.params.virtual.enabled ? e.virtual.slides.length : e.slides.length,
+      P = e.params.loop ? Math.ceil($ / e.params.slidesPerGroup) : e.snapGrid.length;
+    if (e.params.loop ? ((f = e.previousRealIndex || 0), (i = e.params.slidesPerGroup > 1 ? Math.floor(e.realIndex / e.params.slidesPerGroup) : e.realIndex)) : typeof e.snapIndex < "u" ? ((i = e.snapIndex), (f = e.previousSnapIndex)) : ((f = e.previousIndex || 0), (i = e.activeIndex || 0)), t.type === "bullets" && e.pagination.bullets && e.pagination.bullets.length > 0) {
       const n = e.pagination.bullets;
       let g, m, k;
-      if (
-        (t.dynamicBullets &&
-          ((b = K(n[0], e.isHorizontal() ? "width" : "height", !0)),
-          s.forEach((r) => {
-            r.style[e.isHorizontal() ? "width" : "height"] = `${
-              b * (t.dynamicMainBullets + 4)
-            }px`;
-          }),
-          t.dynamicMainBullets > 1 &&
-            f !== void 0 &&
-            ((h += i - (f || 0)),
-            h > t.dynamicMainBullets - 1
-              ? (h = t.dynamicMainBullets - 1)
-              : h < 0 && (h = 0)),
-          (g = Math.max(i - h, 0)),
-          (m = g + (Math.min(n.length, t.dynamicMainBullets) - 1)),
-          (k = (m + g) / 2)),
-        n.forEach((r) => {
-          const c = [
-            ...["", "-next", "-next-next", "-prev", "-prev-prev", "-main"]
-              .map((p) => `${t.bulletActiveClass}${p}`)
-              .map((p) =>
-                typeof p == "string" && p.includes(" ") ? p.split(" ") : p
-              )
-              .flat(),
-          ];
-          r.classList.remove(...c);
-        }),
-        s.length > 1)
-      )
+      if ((t.dynamicBullets && ((b = K(n[0], e.isHorizontal() ? "width" : "height", !0)), s.forEach((r) => {
+        r.style[e.isHorizontal() ? "width" : "height"] = `${b * (t.dynamicMainBullets + 4)}px`;
+      }), t.dynamicMainBullets > 1 && f !== void 0 && ((h += i - (f || 0)), h > t.dynamicMainBullets - 1 ? (h = t.dynamicMainBullets - 1) : h < 0 && (h = 0)), (g = Math.max(i - h, 0)), (m = g + (Math.min(n.length, t.dynamicMainBullets) - 1)), (k = (m + g) / 2)), n.forEach((r) => {
+        const c = [...["", "-next", "-next-next", "-prev", "-prev-prev", "-main"].map((p) => `${t.bulletActiveClass}${p}`)].map((p) => (typeof p == "string" && p.includes(" ") ? p.split(" ") : p)).flat();
+        r.classList.remove(...c);
+      }), s.length > 1))
         n.forEach((r) => {
           const c = D(r);
-          c === i
-            ? r.classList.add(...t.bulletActiveClass.split(" "))
-            : e.isElement && r.setAttribute("part", "bullet"),
-            t.dynamicBullets &&
-              (c >= g &&
-                c <= m &&
-                r.classList.add(...`${t.bulletActiveClass}-main`.split(" ")),
-              c === g && B(r, "prev"),
-              c === m && B(r, "next"));
+          c === i ? r.classList.add(...t.bulletActiveClass.split(" ")) : e.isElement && r.setAttribute("part", "bullet"),
+            t.dynamicBullets && (c >= g && c <= m && r.classList.add(...`${t.bulletActiveClass}-main`.split(" ")), c === g && B(r, "prev"), c === m && B(r, "next"));
         });
       else {
         const r = n[i];
-        if (
-          (r && r.classList.add(...t.bulletActiveClass.split(" ")),
-          e.isElement &&
-            n.forEach((c, p) => {
-              c.setAttribute("part", p === i ? "bullet-active" : "bullet");
-            }),
-          t.dynamicBullets)
-        ) {
+        if ((r && r.classList.add(...t.bulletActiveClass.split(" ")), e.isElement && n.forEach((c, p) => {
+          c.setAttribute("part", p === i ? "bullet-active" : "bullet");
+        }), t.dynamicBullets)) {
           const c = n[g],
             p = n[m];
-          for (let x = g; x <= m; x += 1)
-            n[x] &&
-              n[x].classList.add(...`${t.bulletActiveClass}-main`.split(" "));
+          for (let x = g; x <= m; x += 1) n[x] && n[x].classList.add(...`${t.bulletActiveClass}-main`.split(" "));
           B(c, "prev"), B(p, "next");
         }
       }
@@ -254,90 +200,43 @@ function V(l) {
       }
     }
     s.forEach((n, g) => {
-      if (
-        (t.type === "fraction" &&
-          (n.querySelectorAll(S(t.currentClass)).forEach((m) => {
-            m.textContent = t.formatFractionCurrent(i + 1);
-          }),
-          n.querySelectorAll(S(t.totalClass)).forEach((m) => {
-            m.textContent = t.formatFractionTotal(P);
-          })),
-        t.type === "progressbar")
-      ) {
+      if (t.type === "fraction" && (n.querySelectorAll(S(t.currentClass)).forEach((m) => {
+        m.textContent = t.formatFractionCurrent(i + 1);
+      }), n.querySelectorAll(S(t.totalClass)).forEach((m) => {
+        m.textContent = t.formatFractionTotal(P);
+      })), t.type === "progressbar") {
         let m;
-        t.progressbarOpposite
-          ? (m = e.isHorizontal() ? "vertical" : "horizontal")
-          : (m = e.isHorizontal() ? "horizontal" : "vertical");
+        t.progressbarOpposite ? (m = e.isHorizontal() ? "vertical" : "horizontal") : (m = e.isHorizontal() ? "horizontal" : "vertical");
         const k = (i + 1) / P;
         let r = 1,
           c = 1;
-        m === "horizontal" ? (r = k) : (c = k),
-          n.querySelectorAll(S(t.progressbarFillClass)).forEach((p) => {
-            (p.style.transform = `translate3d(0,0,0) scaleX(${r}) scaleY(${c})`),
-              (p.style.transitionDuration = `${e.params.speed}ms`);
-          });
+        m === "horizontal" ? (r = k) : (c = k), n.querySelectorAll(S(t.progressbarFillClass)).forEach((p) => {
+          p.style.transform = `translate3d(0,0,0) scaleX(${r}) scaleY(${c})`, p.style.transitionDuration = `${e.params.speed}ms`;
+        });
       }
-      t.type === "custom" && t.renderCustom
-        ? ((n.innerHTML = t.renderCustom(e, i + 1, P)),
-          g === 0 && u("paginationRender", n))
-        : (g === 0 && u("paginationRender", n), u("paginationUpdate", n)),
-        e.params.watchOverflow &&
-          e.enabled &&
-          n.classList[e.isLocked ? "add" : "remove"](t.lockClass);
+      t.type === "custom" && t.renderCustom ? (n.innerHTML = t.renderCustom(e, i + 1, P), g === 0 && u("paginationRender", n)) : (g === 0 && u("paginationRender", n), u("paginationUpdate", n)), e.params.watchOverflow && e.enabled && n.classList[e.isLocked ? "add" : "remove"](t.lockClass);
     });
   }
   function A() {
     const a = e.params.pagination;
     if (L()) return;
-    const t =
-      e.virtual && e.params.virtual.enabled
-        ? e.virtual.slides.length
-        : e.grid && e.params.grid.rows > 1
-        ? e.slides.length / Math.ceil(e.params.grid.rows)
-        : e.slides.length;
+    const t = e.virtual && e.params.virtual.enabled ? e.virtual.slides.length : e.grid && e.params.grid.rows > 1 ? e.slides.length / Math.ceil(e.params.grid.rows) : e.slides.length;
     let s = e.pagination.el;
     s = y(s);
     let i = "";
     if (a.type === "bullets") {
-      let f = e.params.loop
-        ? Math.ceil(t / e.params.slidesPerGroup)
-        : e.snapGrid.length;
-      e.params.freeMode &&
-        e.params.freeMode.enabled &&
-        f > t &&
-        (f = t);
-      for (let $ = 0; $ < f; $ += 1)
-        a.renderBullet
-          ? (i += a.renderBullet.call(e, $, a.bulletClass))
-          : (i += `<${a.bulletElement} ${
-              e.isElement ? 'part="bullet"' : ""
-            } class="${a.bulletClass}"></${a.bulletElement}>`);
+      let f = e.params.loop ? Math.ceil(t / e.params.slidesPerGroup) : e.snapGrid.length;
+      e.params.freeMode && e.params.freeMode.enabled && f > t && (f = t);
+      for (let $ = 0; $ < f; $ += 1) a.renderBullet ? (i += a.renderBullet.call(e, $, a.bulletClass)) : (i += `<${a.bulletElement} ${e.isElement ? 'part="bullet"' : ""} class="${a.bulletClass}"></${a.bulletElement}>`);
     }
-    a.type === "fraction" &&
-      (a.renderFraction
-        ? (i = a.renderFraction.call(e, a.currentClass, a.totalClass))
-        : (i = `<span class="${a.currentClass}"></span> / <span class="${a.totalClass}"></span>`)),
-      a.type === "progressbar" &&
-        (a.renderProgressbar
-          ? (i = a.renderProgressbar.call(e, a.progressbarFillClass))
-          : (i = `<span class="${a.progressbarFillClass}"></span>`)),
-      (e.pagination.bullets = []),
-      s.forEach((f) => {
-        a.type !== "custom" && (f.innerHTML = i || ""),
-          a.type === "bullets" &&
-            e.pagination.bullets.push(...f.querySelectorAll(S(a.bulletClass)));
-      }),
-      a.type !== "custom" && u("paginationRender", s[0]);
+    a.type === "fraction" && (a.renderFraction ? (i = a.renderFraction.call(e, a.currentClass, a.totalClass)) : (i = `<span class="${a.currentClass}"></span> / <span class="${a.totalClass}"></span>`)), a.type === "progressbar" && (a.renderProgressbar ? (i = a.renderProgressbar.call(e, a.progressbarFillClass)) : (i = `<span class="${a.progressbarFillClass}"></span>`)), (e.pagination.bullets = []), s.forEach((f) => {
+      a.type !== "custom" && (f.innerHTML = i || ""), a.type === "bullets" && e.pagination.bullets.push(...f.querySelectorAll(S(a.bulletClass)));
+    }), a.type !== "custom" && u("paginationRender", s[0]);
   }
   function I() {
-    e.params.pagination = j(
-      e,
-      e.originalParams.pagination,
-      e.params.pagination,
-      {
-        el: "swiper-pagination",
-      }
-    );
+    e.params.pagination = j(e, e.originalParams.pagination, e.params.pagination, {
+      el: "swiper-pagination",
+    });
     const a = e.params.pagination;
     if (!a.el) return;
     let t;
@@ -357,19 +256,11 @@ function V(l) {
         }),
         (t = y(t)),
         t.forEach((s) => {
-          a.type === "bullets" &&
-            a.clickable &&
-            s.classList.add(...(a.clickableClass || "").split(" ")),
+          a.type === "bullets" && a.clickable && s.classList.add(...(a.clickableClass || "").split(" ")),
             s.classList.add(a.modifierClass + a.type),
             s.classList.add(e.isHorizontal() ? a.horizontalClass : a.verticalClass),
-            a.type === "bullets" &&
-              a.dynamicBullets &&
-              (s.classList.add(`${a.modifierClass}${a.type}-dynamic`),
-              (h = 0),
-              a.dynamicMainBullets < 1 && (a.dynamicMainBullets = 1)),
-            a.type === "progressbar" &&
-              a.progressbarOpposite &&
-              s.classList.add(a.progressbarOppositeClass),
+            a.type === "bullets" && a.dynamicBullets && (s.classList.add(`${a.modifierClass}${a.type}-dynamic`), (h = 0), a.dynamicMainBullets < 1 && (a.dynamicMainBullets = 1)),
+            a.type === "progressbar" && a.progressbarOpposite && s.classList.add(a.progressbarOppositeClass),
             a.clickable && s.addEventListener("click", z),
             e.enabled || s.classList.add(a.lockClass);
         }));
@@ -384,23 +275,17 @@ function V(l) {
         s.classList.remove(a.hiddenClass),
           s.classList.remove(a.modifierClass + a.type),
           s.classList.remove(e.isHorizontal() ? a.horizontalClass : a.verticalClass),
-          a.clickable &&
-            (s.classList.remove(...(a.clickableClass || "").split(" ")),
-            s.removeEventListener("click", z));
+          a.clickable && (s.classList.remove(...(a.clickableClass || "").split(" ")), s.removeEventListener("click", z));
       })),
-      e.pagination.bullets &&
-        e.pagination.bullets.forEach((s) =>
-          s.classList.remove(...a.bulletActiveClass.split(" "))
-        );
+      e.pagination.bullets && e.pagination.bullets.forEach((s) => s.classList.remove(...a.bulletActiveClass.split(" ")));
   }
   d("changeDirection", () => {
     if (!e.pagination || !e.pagination.el) return;
     const a = e.params.pagination;
     let { el: t } = e.pagination;
-    (t = y(t)),
+    t = y(t),
       t.forEach((s) => {
-        s.classList.remove(a.horizontalClass, a.verticalClass),
-          s.classList.add(e.isHorizontal() ? a.horizontalClass : a.verticalClass);
+        s.classList.remove(a.horizontalClass, a.verticalClass), s.classList.add(e.isHorizontal() ? a.horizontalClass : a.verticalClass);
       });
   }),
     d("init", () => {
@@ -422,9 +307,7 @@ function V(l) {
       let { el: a } = e.pagination;
       a &&
         ((a = y(a)),
-        a.forEach((t) =>
-          t.classList[e.enabled ? "remove" : "add"](e.params.pagination.lockClass)
-        ));
+        a.forEach((t) => t.classList[e.enabled ? "remove" : "add"](e.params.pagination.lockClass)));
     }),
     d("lock unlock", () => {
       v();
@@ -432,18 +315,8 @@ function V(l) {
     d("click", (a, t) => {
       const s = t.target,
         i = y(e.pagination.el);
-      if (
-        e.params.pagination.el &&
-        e.params.pagination.hideOnClick &&
-        i &&
-        i.length > 0 &&
-        !s.classList.contains(e.params.pagination.bulletClass)
-      ) {
-        if (
-          e.navigation &&
-          ((e.navigation.nextEl && s === e.navigation.nextEl) ||
-            (e.navigation.prevEl && s === e.navigation.prevEl))
-        )
+      if (e.params.pagination.el && e.params.pagination.hideOnClick && i && i.length > 0 && !s.classList.contains(e.params.pagination.bulletClass)) {
+        if (e.navigation && (e.navigation.nextEl && s === e.navigation.nextEl || e.navigation.prevEl && s === e.navigation.prevEl))
           return;
         const f = i[0].classList.contains(e.params.pagination.hiddenClass);
         u(f === !0 ? "paginationShow" : "paginationHide"),
@@ -455,9 +328,7 @@ function V(l) {
       let { el: a } = e.pagination;
       a &&
         ((a = y(a)),
-        a.forEach((t) =>
-          t.classList.remove(e.params.pagination.paginationDisabledClass)
-        )),
+        a.forEach((t) => t.classList.remove(e.params.pagination.paginationDisabledClass))),
         I(),
         A(),
         v();
@@ -467,9 +338,7 @@ function V(l) {
       let { el: a } = e.pagination;
       a &&
         ((a = y(a)),
-        a.forEach((t) =>
-          t.classList.add(e.params.pagination.paginationDisabledClass)
-        )),
+        a.forEach((t) => t.classList.add(e.params.pagination.paginationDisabledClass))),
         M();
     };
   Object.assign(e.pagination, {
